e83413562b296c66fb8b53c3dfcb0c59688d6b06
Timmyy3000 <59262737+Timmyy3000@users.noreply.github.com>
2021-01-04 20:22:32 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+SIZE = 192
+mode = st.checkbox("Draw (or Delete)?", True)
+canvas_result = st_canvas(
+    fill_color='#000000',
+    stroke_width=20,
+    stroke_color='#FFFFFF',
+    background_color='#000000',
+    width=SIZE,
+    height=SIZE,
+    drawing_mode="freedraw" if mode else "transform",
+    key='canvas')
+
+if canvas_result.image_data is not None:
+    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+    rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+    st.write('Model Input')
+    st.image(rescaled)
